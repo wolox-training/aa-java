def944b08c8cc833be8d250311d1b06f5013a5cd
AguirreAlexa <aguirre.alexa.c@gmail.com>
2022-08-17 09:42:46 -0300

    Fixing error loading ApplicationContext

--- src/main/java/wolox/training/TrainingApplication.java
+++ src/main/java/wolox/training/TrainingApplication.java
@@ -1,6 +1,7 @@
 package wolox.training;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
--- src/test/java/wolox/training/controllers/BookControllerTest.java
+++ src/test/java/wolox/training/controllers/BookControllerTest.java
@@ -1,23 +1,53 @@
 package wolox.training.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import wolox.training.models.Book;
 import wolox.training.repositories.BookRepository;
+import wolox.training.repositories.UserRepository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
 @WebMvcTest(BookController.class)
 class BookControllerTest {
+
     @Autowired
     private MockMvc mvc;
 
     @MockBean
-    private BookRepository mockBookRepository;
+    private BookRepository bookRepository;
+
+    @MockBean
+    private UserRepository userRepository;
     private Book bookTest;
+    private List<Book> listBooks = new ArrayList<>();
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @BeforeEach
     void setUp() {
@@ -30,6 +60,23 @@ class BookControllerTest {
         bookTest.setPublisher("Viking Press");
         bookTest.setYear("1987");
         bookTest.setPages(420);
-        bookTest.setIsbn("09780582402751");
+        bookTest.setIsbn("9780582402751");
+
+        listBooks.add(bookTest);
+    }
+
+    @Test
+    void givenBooks_whenGetBooks_thenReturnJsonArray() throws Exception {
+        String jsonBooks = mapper.writeValueAsString(listBooks);
+
+        given(bookRepository.findAll()).willReturn(listBooks);
+
+        String result = mvc.perform(MockMvcRequestBuilders.get("/api/books")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        assertEquals(jsonBooks, result);
     }
 }
